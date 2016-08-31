d93b9f6e48ab504cc639bc9c5ac4e63cc84835b8
minh5 <minh.v.mai@gmail.com>
2016-08-31 15:50:52 -0400

    added some more logic for blog page

--- gitsite/blog/views.py
+++ gitsite/blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Entry
 
 # Create your views here.
 def entry_list(request):
-    return render(request, 'entry/entry_list.html', {})
+    entries = Entry.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'entry/entry_list.html', {'entries': entries})
