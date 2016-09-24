0f72b34203e43591de39dafe1201d026c654116b
minh5 <minh.v.mai@gmail.com>
2016-09-24 17:58:35 -0400

    added more content

--- gitsite/blog/urls.py
+++ gitsite/blog/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.entry_list, name='entry_list'),
+    url(r'^$', views.home_view, name='home'),
+    url(r'^about', views.about_view, name='about'),
+    url(r'^entry/', views.entry_list, name='entry_list')
 ]
